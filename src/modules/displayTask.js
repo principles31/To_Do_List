48c99876a1fc243590ef878a887a81c6f77c8dd8
principles31 <principles31@gmail.com>
2022-05-12 06:36:56 +0000

    Add modules in src file

---
+++ src/modules/displayTask.js
@@ -0,0 +1,20 @@
+function displayTask() {
+    const todoList = document.querySelector('.todo-list');
+    const localData = JSON.parse(localStorage.getItem('tasks'));
+    if (localData !== null) {
+      localData.forEach((task) => {
+        todoList.innerHTML += `
+              <div class="edit-task" data-key="${task.index}"> 
+              <ul class="ul-edit" >
+                  <li class="tick" ><input id="${task.index}" type="checkbox"></li>
+                  <li class"text-value" >${task.text}</li>
+                  <li class="remove-btn" id="${task.index}"><i class="fa-solid fa-ellipsis-vertical fa-lg vertical-dot"></i></li>
+              </ul>  
+              <hr>
+              </div>
+              `;
+      });
+    }
+  }
+  
+  export default displayTask;
